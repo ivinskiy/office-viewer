2e874ccecc372c62565ced9b93e7d0fb909fb26e
Valle Ivinskiy <71266129+ivinskiy@users.noreply.github.com>
2023-12-04 20:17:22 +0100

    added complete movement to transform controls

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,22 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import { Box } from "./components/Box/Box";
-import {
-  CameraControls,
-  OrbitControls,
-  PerspectiveCamera,
-  Plane,
-} from "@react-three/drei";
+import { CameraControls, PerspectiveCamera } from "@react-three/drei";
 import * as THREE from "three";
+import { Plane } from "./components/Plane/Plane";
+import {
+  EffectComposer,
+  Outline,
+  Selection,
+} from "@react-three/postprocessing";
 
 function App() {
-  const [count, setCount] = useState(0);
   const controlsRef = useRef<CameraControls | null>(null);
   const cameraRef = useRef<THREE.PerspectiveCamera | null>(null);
 
   return (
     <div style={{ width: "100vw", height: "75vh" }}>
-      <Canvas>
+      <Canvas shadows>
         <PerspectiveCamera
           ref={cameraRef}
           position={[0, 5, 5]}
@@ -28,14 +28,22 @@ function App() {
         />
         <color attach="background" args={[0, 0, 0]} />
         <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-        <Plane
-          rotation={[-Math.PI / 2, 0, 0]}
-          args={[10, 10]}
-          material={new THREE.MeshBasicMaterial({ color: "green" })}
-        />
+        <pointLight position={[5, 5, 5]} castShadow intensity={150} />
+        <Selection>
+          <EffectComposer multisampling={8} autoClear={false}>
+            <Outline
+              blur
+              visibleEdgeColor={0xff0000}
+              edgeStrength={100}
+              width={1000}
+              xRay={false}
+            />
+          </EffectComposer>
+          <Box position={[-1.2, 0.5, 0]} orbitRef={controlsRef} />
+          <Box position={[1.2, 0.5, 0]} orbitRef={controlsRef} />
+        </Selection>
+        <Plane />
+
         <CameraControls ref={controlsRef} maxPolarAngle={Math.PI / 2 - 0.1} />
       </Canvas>
       <button
--- src/components/Plane/Plane.tsx
+++ src/components/Plane/Plane.tsx
@@ -1,8 +1,13 @@
 export const Plane = () => {
   return (
-    <mesh position={[0, 0, 0]} rotation={[Math.PI / 2, 0, 0]} scale={[1, 1, 1]}>
-      <planeGeometry />
-      <meshBasicMaterial color="green" />
+    <mesh
+      position={[0, 0, 0]}
+      rotation={[-Math.PI / 2, 0, 0]}
+      scale={[1, 1, 1]}
+      receiveShadow
+    >
+      <planeGeometry args={[10, 10]} />
+      <meshBasicMaterial color="gray" />
     </mesh>
   );
 };
